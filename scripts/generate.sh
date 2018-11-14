91b35ff63ce86b650e4db530e2ad5d5651016d7e
Vicenc Garcia-Altes <vicentredrum@gmail.com>
2018-11-14 11:54:40 +0000

    Fix generate.sh

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -138,5 +138,5 @@ RUN apt-get install lsb-release \
   && AZ_REPO=$(lsb_release -cs) \
   && echo 'deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main' | sudo tee /etc/apt/sources.list.d/azure-cli.list \
   && curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add - \
-  && apt-get update
+  && apt-get update \
   && pt-get install libssl-dev libffi-dev python-dev apt-transport-https azure-cli"
