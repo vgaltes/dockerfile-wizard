72338dfb82f8acc987e9e816f8835916db75b081
Vicenc Garcia-Altes <vicentredrum@gmail.com>
2018-11-14 13:34:10 +0000

    remove sudo

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -136,7 +136,7 @@ fi
 echo "# install Azure CLI
 RUN apt-get install lsb-release \
   && AZ_REPO=$(lsb_release -cs) \
-  && echo 'deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main' | sudo tee /etc/apt/sources.list.d/azure-cli.list \
-  && curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add - \
+  && echo 'deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main' | tee /etc/apt/sources.list.d/azure-cli.list \
+  && curl -L https://packages.microsoft.com/keys/microsoft.asc | apt-key add - \
   && apt-get update \
   && pt-get install libssl-dev libffi-dev python-dev apt-transport-https azure-cli"
